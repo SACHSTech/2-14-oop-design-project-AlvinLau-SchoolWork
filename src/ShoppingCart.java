1a8ebafe0d1baa90d7df544221abfe649fca9618
AlvinLau-SchoolWork <chunhin.lau24@ycdsbk12.ca>
2025-05-27 13:32:30 -0400

    Added getter and setter methods for ShoppingCart class

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -6,4 +6,20 @@ public class ShoppingCart {
     public ShoppingCart() {
         this.items = new ArrayList<>();
     }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void addItem(Product product, int quantity) {
+        items.add(new CartItem(product, quantity));
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (CartItem item : items) {
+            total += item.getLineTotal();
+        }
+        return total;
+    }
 }
