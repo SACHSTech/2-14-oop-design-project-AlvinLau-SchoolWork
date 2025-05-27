0dff0215999e49478b42c3bc285fa0d3ac661b59
AlvinLau-SchoolWork <chunhin.lau24@ycdsbk12.ca>
2025-05-27 11:33:10 -0400

    Added getter and setter methods for Product class

--- src/Product.java
+++ src/Product.java
@@ -8,4 +8,24 @@ public class Product {
         this.name = name;
         this.price = price;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
