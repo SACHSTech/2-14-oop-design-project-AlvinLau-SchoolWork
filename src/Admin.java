c6db258d00a3505774f8732b4119d9710ead6cde
AlvinLau-SchoolWork <chunhin.lau24@ycdsbk12.ca>
2025-05-28 07:35:41 -0400

    Added new admin class

---
+++ src/Admin.java
@@ -0,0 +1,10 @@
+public class Admin extends User {
+    public Admin(String id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void displayRole() {
+        System.out.println("Admin");
+    }
+}
