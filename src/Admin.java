8979ce7c5290c85a8f1833897744bf1cdf7e1ffe
AlvinLau-SchoolWork <chunhin.lau24@ycdsbk12.ca>
2025-05-28 07:57:09 -0400

    Added getter setter methods, add/remove method for Admin class

--- src/Admin.java
+++ src/Admin.java
@@ -3,6 +3,22 @@ public class Admin extends User {
         super(id, name);
     }
 
+    public int getQuantity(Stock stock, Product p) {
+        return stock.getQuantity(p);
+    }
+
+    public void setProductStock(Stock stock, Product p, int quantity) {
+        stock.setQuantity(p, quantity);
+    }
+
+    public void addProductStock(Stock stock, Product p, int quantity) {
+        stock.increaseQuantity(p, quantity);
+    }
+
+    public boolean removeProductStock(Stock stock, Product p, int quantity) {
+        return stock.removeQuantity(p, quantity);
+    }
+
     @Override
     public void displayRole() {
         System.out.println("Admin");
